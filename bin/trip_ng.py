3ee0b4d2b17efb08cee71512a2122f995a6a1f79
elessar@numenor.org <elessar@numenor.org@d5485f00-391e-ac97-d343-b48165702b25>
2011-10-10 05:33:44 +0000

    Initial code commit; no, it's not working right now (I hate import issues).
    git-svn-id: https://trip-ng.googlecode.com/svn/trunk@10 d5485f00-391e-ac97-d343-b48165702b25

---
+++ bin/trip_ng.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2011 Kenneth Lareau, Nicholas Long
+#
+# All rights reserved.
+
+"""TRipNG is an application to rip and encode CDs into various digital
+   formats.  It has the ability to handle multiple rippers and encoders
+   and allows the user to tag their music in a flexible manner.
+"""
+
+import sys
+
+# Only latest version of Python 2.x (2.7) is supported
+pyvers = sys.version_info[:2]
+
+if pyvers < (2, 7):
+    raise RuntimeError('Python 2.7 is required to use this program')
+
+if pyvers[0] == 3:
+    raise RuntimeError('Python 3.x is not supported at this time, please '
+                       'use Python 2.7')
+
+
+# We have the right Python version, move along...
+import argparse
+import os.path
+
+# Temporary for now... maybe?
+basedir = os.path.realpath(os.path.dirname(os.path.dirname(sys.argv[0])))
+sys.path.insert(0, os.path.join(basedir, 'lib', 'python'))
+
+import trip_ng.tripconf as tripconf
+
+parser = argparse.ArgumentParser(description='Program to rip and encode CDs')
+parser.add_argument('--device', '-d')
+parser.add_argument('--ripper', '-r', choices=tripconf.rippers)
+parser.add_argument('--encoder', '-e', nargs='+', choices=tripconf.encoders)
+parser.add_argument('--normalize', '-n', action='store_true', default=False)
+parser.add_argument('--retain-lossless', action='store_true', default=False)
---
+++ lib/python/trip_ng/tripconf.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2011 Kenneth Lareau, Nicholas Long
+#
+# All rights reserved.
+
+"""Basic configuration information for TRipNG application"""
+
+rippers = [ 'cdda2wav', 'cdparanoia' ]
+encoders = [ 'lame', 'oggenc', 'flac' ]
