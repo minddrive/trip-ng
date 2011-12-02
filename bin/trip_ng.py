6c4d3288cfd3a752fa9cd326589869a827b09e7f
elessar@numenor.org <elessar@numenor.org@d5485f00-391e-ac97-d343-b48165702b25>
2011-12-02 05:14:25 +0000

    A bit of manual unit testing will do wonders; additional code to handle more edge cases and catch various configuration and command line errors.
    git-svn-id: https://trip-ng.googlecode.com/svn/trunk@16 d5485f00-391e-ac97-d343-b48165702b25

--- bin/trip_ng.py
+++ bin/trip_ng.py
@@ -36,48 +36,68 @@ import trip_ng.tripconf as tripconf
 
 def parse_command_line():
     """ """
-    
+
     conf_error = False
-    
+
     conf_parser = argparse.ArgumentParser(add_help=False)
     conf_parser.add_argument('--configuration', '-c', metavar='FILE',
-                             default=tripconf.default_conf)
-    args, remaining_argv = conf_parser.parse_known_args()
+                             default=tripconf.default_conf,
+                             help='Configuration file to use')
+    conf_args, remaining_argv = conf_parser.parse_known_args()
 
     defaults = { 'ripper' : 'cdda2wav',
                  'encoder' : [ 'lame' ], }
 
     try:
-        with open(args.configuration) as conf_file:
+        with open(conf_args.configuration) as conf_file:
             config = ConfigParser.SafeConfigParser()
-            config.read(conf_file)
-            defaults.update(config.items('TripNG'))
+            config.readfp(conf_file)
+            defaults.update(config.items('TRipNG'))
+
+            # Make sure 'encoder' variable is a list
+            defaults['encoder'] = list(set(defaults['encoder'].split()))
     except IOError:
         # No profile if config file doesn't exist UNLESS configuration
         # file is specified; raise error after full parser is set up
         # so user sees full help information
-        if args.configuration != tripconf.default_conf:
+        if conf_args.configuration != tripconf.default_conf:
             conf_error = True
-    
+
     parser = argparse.ArgumentParser(parents=[ conf_parser ],
                                  description='Program to rip and encode CDs')
 
-    parser.set_defaults(**defaults)
-    parser.add_argument('--device', '-d')
-    parser.add_argument('--ripper', '-r', choices=tripconf.rippers)
-    parser.add_argument('--encoder', '-e', nargs='+', choices=tripconf.encoders)
-    parser.add_argument('--normalize', '-n', action='store_true', default=False)
-    parser.add_argument('--retain-lossless', action='store_true', default=False)
+    parser.add_argument('--device', '-d', help='Media device to use')
+    parser.add_argument('--ripper', '-r', choices=tripconf.rippers,
+                        help='Ripping program to use')
+    parser.add_argument('--encoder', '-e', nargs='+',
+                        choices=tripconf.encoders,
+                        help='Encoding program(s) to use (multiple allowed)')
+    parser.add_argument('--normalize', '-n', action='store_true',
+                        default=False, help='Use normalization')
+    parser.add_argument('--retain-lossless', action='store_true',
+                        default=False, help='Keep lossless files')
 
     if conf_error:
         parser.error('Configuration file %s does not exist'
                      % args.configuration)
 
-    return parser.parse_args(remaining_argv)
+    parser.set_defaults(**defaults)
+    args = parser.parse_args(remaining_argv)
+
+    # Now verify some of the new defaults, since argparse blindly
+    # applies them without verifying they're valid for the options
+    encoders = set(args.encoder)
+
+    if args.ripper not in tripconf.rippers:
+        parser.error('%s not a valid ripper' % args.ripper)
+
+    if not encoders.issubset(set(tripconf.encoders)):
+        parser.error('%s not a valid set of encoders' % args.encoder)
+
+    return args
 
 
 if __name__ == '__main__':
-    
+
     args = parse_command_line()
     print args
-    
